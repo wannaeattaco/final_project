44b9b819d446f073c8154f0aef4c99ea8cba0a41
Thunyanan Tangpipatpong <thunynan.t@ku.th>
2023-12-16 23:46:36 +0700

    add more class

--- project_manage.py
+++ project_manage.py
@@ -41,7 +41,7 @@ def initializing():
     db.insert(project_pending_table)
 
     # project table
-    project = ReadCsv('project_request.csv')
+    project = ReadCsv('project.csv')
     ReadCsv.read_csv(project)
     project_table = Table('project', project.data)
     db.insert(project_table)
@@ -60,9 +60,9 @@ def login():
     username = input("Enter your username here: ")
     password = input("Enter your password here: ")
     login_all = db.search('login')
-    for i in login_all.table:
-        if username == i['username'] and password == i['password']:
-            return [i['ID'], i['role']]
+    for login_info in login_all.table:
+        if username == login_info['username'] and password == login_info['password']:
+            return [login_info['ID'], login_info['role']]
     return None
 # here are things to do in this function:
     # add code that performs a login task
@@ -88,39 +88,82 @@ class Student:
         self.student_last = student_last
 
     def view_request(self):
-        pass
+        request = db.search('member_pending')
+        request_data = request.table.filter(lambda x: x['ID'] == self.student_id)
+        print(request_data)
 
     def accept_deny_request(self):
-        pass
+        request_table = db.search('member_pending')
+        project_table = db.search('project')
+        project_id = request_table['ID']
+        response = input("Do you want to accept project invitation? (yes or no):")
+        request_table.update({'ProjectID': project_id}, {'Response': response})
+        if project_table['Member1'] is None:
+            project_table.update({'ProjectID': project_id}, {'Member1': self.student_id})
+        else:
+            project_table.update({'ProjectID': project_id}, {'Member2': self.student_id})
+        # print(f"{} invitation accepted")
 
     def change_into_lead(self):
-        pass
+        persons_table = db.search('persons')
+        persons_table.update({'ID': self.student_id}, {'role': 'lead'})
 
     def view_project(self):
-        pass
+        project_table = db.search('project')
+        choice = input("Which project you want to view (view all(a), by name(n)): ")
+        if choice.lower() == 'a':
+            for project in project_table:
+                project(f"Project number {project}")
+                print(f"Project ID: {project['ProjectID']}")
+                print(f"Title: {project['Title']}")
+                print(f"Lead: {project['Lead']}")
+                print(f"Member no.1 {project['Member1']}")
+                print(f"Member no.2 {project['Member2']}")
+                print(f"Advisor: {project['Advisor']}")
+                print(f"Status: {project['Status']}")
+        if choice.lower() == 'n':
+            name = input("Enter project name: ")
+            for project in project_table:  # change to while
+                if name == project['Title']:
+                    print(f"Project ID: {project['ProjectID']}")
+                    print(f"Title: {project['Title']}")
+                    print(f"Lead: {project['Lead']}")
+                    print(f"Member no.1 {project['Member1']}")
+                    print(f"Member no.2 {project['Member2']}")
+                    print(f"Advisor: {project['Advisor']}")
+                    print(f"Status: {project['Status']}")
 
     def modify_project(self):
-        pass
+        project_table = db.search('project')
+        project_id = input("Please enter the project ID that you want to verify: ")
+        project_table.update({'ProjectID': project_id}, update_data)
 
-    def create_project(self):
-        pass
+    def create_project(self, project_id, title, lead, member1="", member2="", advisor="",status=""):
+        project_table = db.search('project')
+        project_table.insert({'projectID': project_id, 'title': title,
+                              'lead': lead, 'member1': member1, 'member2': member2, 'advisor': advisor, 'status': status})
 
 
 class Lead(Student):
-    def __init__(self, student_id: str, student_first: str, student_last: str):
+    def __init__(self, student_id: str, student_first: str, student_last: str, project_id=""):
         super().__init__(student_id, student_first, student_last)
+        self.project_id = project_id
 
     def project_status(self):
         pass
 
-    def request_status(self):
-        pass
+    def view_request(self):
+        request = db.search('member_pending')
+        request_data = request.table.filter(lambda x: x['ID'] == self.student_id)
+        print(request_data)
 
-    def send_member_request(self):
-        pass
+    def send_member_request(self, member_id):
+        member_request_table = db.search('member_pending')
+        member_request_table.insert({'ProjectID': self.project_id, 'to_be_member': member_id})
 
-    def send_advisor_request(self):
-        pass
+    def send_advisor_request(self, advisor_id):
+        member_request_table = db.search('member_pending')
+        member_request_table.insert({'ProjectID': self.project_id, 'to_be_member': advisor_id})
 
 
 class Member(Student):
@@ -131,7 +174,10 @@ class Member(Student):
         pass
 
     def request_status(self):
-        pass
+        request_table = db.search('member_pending')
+        for request in request_table.table:
+            if request['to_be_member'] == self.student_id:
+                print(request)
 
 
 class Faculty:
@@ -141,10 +187,17 @@ class Faculty:
         self.faculty_last = faculty_last
 
     def view_request(self):
-        pass
+        request = db.search('advisor_pending')
+        request_data = request.table.filter(lambda x: x['ID'] == self.faculty_id)
+        print(request_data)
 
     def accept_deny_request(self):
-        pass
+        request_table = db.search('member_pending')
+        project_table = db.search('project')
+        project_id = request_table['ID']
+        response = input("Do you want to accept project invitation? (yes or no):")
+        request_table.update({'ProjectID': project_id}, {'Response': response})
+        project_table.update({'ProjectID': project_id}, {'Member1': self.faculty_id})
 
     def view_project(self):
         pass
@@ -177,7 +230,10 @@ class Admin:
         self.admin_last = admin_last
 
     def update_table(self):
-        pass
+        table = db.search(table_name)
+        if table:
+            for data in update_data:
+                table.update(data['identifier_key'], data['identifier_value'], data['update'])
 
     def modify_table(self):
         pass
