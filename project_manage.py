2bfb9ad63604185daba150790e35101fdad9194d
Thunyanan Tangpipatpong <thunynan.t@ku.th>
2023-12-16 13:18:58 +0700

    add classes

--- project_manage.py
+++ project_manage.py
@@ -1,4 +1,4 @@
-# import database module
+# import database module 
 from database import *
 # import copy
 
@@ -8,10 +8,10 @@ db = Database()
 
 def initializing():
     # persons table
-    person_data = ReadCsv('persons.csv')
-    ReadCsv.read_csv(person_data)
-    persons_table = Table('persons', person_data.data)
-    db.insert(persons_table)
+    advisor_pending = ReadCsv('persons.csv')
+    ReadCsv.read_csv(advisor_pending)
+    advisor_pending_table = Table('persons', advisor_pending.data)
+    db.insert(advisor_pending_table)
     # create a 'persons' table
     # add the 'persons' table into the database
     # for row in person_data:
@@ -21,6 +21,30 @@ def initializing():
     ReadCsv.read_csv(login_data)
     login_table = Table('login', login_data.data)  # adding dict in list
     db.insert(login_table)
+
+    # advisor pending table
+    advisor_pending = ReadCsv('advisor_pending_request.csv')
+    ReadCsv.read_csv(advisor_pending)
+    advisor_pending_table = Table('advisor_pending', advisor_pending.data)
+    db.insert(advisor_pending_table)
+
+    # member pending table
+    member_pending = ReadCsv('member_pending_request.csv')
+    ReadCsv.read_csv(member_pending)
+    member_pending_table = Table('member_pending', member_pending.data)
+    db.insert(member_pending_table)
+
+    # project pending table
+    project_pending = ReadCsv('project_pending_request.csv')
+    ReadCsv.read_csv(project_pending)
+    project_pending_table = Table('project_pending', project_pending.data)
+    db.insert(project_pending_table)
+
+    # project table
+    project = ReadCsv('project_request.csv')
+    ReadCsv.read_csv(project)
+    project_table = Table('project', project.data)
+    db.insert(project_table)
 # here are things to do in this function:
     # create an object to read all csv files that will serve as a persistent state for this program
 
@@ -57,24 +81,133 @@ def exit():
 # https://www.pythonforbeginners.com/basics/list-of-dictionaries-to-csv-in-python
 
 
+class Student:
+    def __init__(self, student_id: str, student_first: str, student_last: str):
+        self.student_id = student_id
+        self.student_first = student_first
+        self.student_last = student_last
+
+    def view_request(self):
+        pass
+
+    def accept_deny_request(self):
+        pass
+
+    def change_into_lead(self):
+        pass
+
+    def view_project(self):
+        pass
+
+    def modify_project(self):
+        pass
+
+    def create_project(self):
+        pass
+
+
+class Lead(Student):
+    def __init__(self, student_id: str, student_first: str, student_last: str):
+        super().__init__(student_id, student_first, student_last)
+
+    def project_status(self):
+        pass
+
+    def request_status(self):
+        pass
+
+    def send_member_request(self):
+        pass
+
+    def send_advisor_request(self):
+        pass
+
+
+class Member(Student):
+    def __init__(self, student_id: str, student_first: str, student_last: str):
+        super().__init__(student_id, student_first, student_last)
+
+    def project_status(self):
+        pass
+
+    def request_status(self):
+        pass
+
+
+class Faculty:
+    def __init__(self, faculty_id, faculty_first, faculty_last):
+        self.faculty_id = faculty_id
+        self.faculty_first = faculty_first
+        self.faculty_last = faculty_last
+
+    def view_request(self):
+        pass
+
+    def accept_deny_request(self):
+        pass
+
+    def view_project(self):
+        pass
+
+    def evaluate_project(self):
+        pass
+
+
+class Advisor(Faculty):
+    def __init__(self, faculty_id, faculty_first, faculty_last):
+        super().__init__(faculty_id, faculty_first, faculty_last)
+
+    def see_project_request(self):
+        pass
+
+    def accept_deny_project(self):
+        pass
+
+    def evaluate_project(self):
+        pass
+
+    def approve_refuse_project(self):
+        pass
+
+
+class Admin:
+    def __init__(self, admin_id, admin_first, admin_last):
+        self.admin_id = admin_id
+        self.admin_first = admin_first
+        self.admin_last = admin_last
+
+    def update_table(self):
+        pass
+
+    def modify_table(self):
+        pass
+
 # make calls to the initializing and login functions defined above
+
 
 initializing()
 val = login()
 
 "based on the return value for login, activate the code that performs activities according to the role defined for that person_id"
-# if val[1] = 'admin':
-#     see and do admin related activities
-# elif val[1] = 'student':
-#     see and do student related activities
-# elif val[1] = 'member':
-#     see and do member related activities
-# elif val[1] = 'lead':
-#     see and do lead related activities
-# elif val[1] = 'faculty':
-#     see and do faculty related activities
-# elif val[1] = 'advisor':
-#     see and do advisor related activities
+person_info = db.search('persons')
+info = []
+for i in person_info.table:
+    if val[0] == i['ID']:
+        info.append(i['ID'])
+        info.append(i['first'])
+        info.append(i['last'])
+if val[1] == 'admin':
+    person = Admin(info[0], info[1], info[2])
+elif val[1] == 'student':
+    person = Student(info[0], info[1], info[2])
+elif val[1] == 'member':
+    person = Member(info[0], info[1], info[2])
+elif val[1] == 'lead':
+    person = Lead(info[0], info[1], info[2])
+elif val[1] == 'faculty':
+    person = Faculty(info[0], info[1], info[2])
+elif val[1] == 'advisor':
+    person = Advisor(info[0], info[1], info[2])
 
 # once every thing is done, make a call to the exit function
 exit()
